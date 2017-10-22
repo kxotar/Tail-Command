bec0df17925ed44365e078c7e1dde597a7e96979
shhhRakesh <sharma26.riti@gmail.com>
2017-10-23 01:49:22 +0530

    Multifile handling

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 #!/usr/bin/env node
 var sys = require('util')
 var spawn = require('child_process').spawn;
-console.log(process.argv)
+//console.log(process.argv)
+list=process.argv;
+list.splice(0,2)
+list.forEach(function (filename) {
+    if (!filename){
+        console.log("File Not Found");
+    }else {
+        var tail = spawn("tail", ["-f", filename]);
+        tail.stdout.on("data", function (data) {
+            console.log("==> "+ filename + " <==");
+            sys.puts(data);
+        });
+    }
 
-var filename = process.argv[2];
-
-if (!filename)
-    return sys.puts("Usage: node myfile");
-
-var tail = spawn("tail", ["-f", filename]);
-sys.puts("start tailing");
-
-tail.stdout.on("data", function (data) {
-    sys.puts(data);
 });
